deed8dec1e153800569fe1fb344735655dc69d79
tianziquan <767050075@qq.com>
2019-11-13 10:15:21 +0800

    update .gitignore

---
+++ src/com/tzqTest/test/math/MathTest.java
@@ -0,0 +1,22 @@
+package com.tzqTest.test.math;
+
+import org.junit.Test;
+
+/**
+ * @author: tiantziquan
+ * @create: 2019-11-11 16:17
+ */
+public class MathTest {
+
+
+    @Test
+    public void testMath(){
+        int currentPage=1;
+        int showCount=10;
+        currentPage=currentPage / showCount;
+        System.out.println("currentPage = " + currentPage);
+        currentPage += 1;
+        System.out.println("currentPage2 = " + currentPage);
+    }
+
+}
